403a18d869c7a502e759c46a33e16e9344eaab54
Takashi Mizohata <beatak@gmail.com>
2012-08-16 01:29:04 -0400

    changes for demo

--- assets/index.js
+++ assets/index.js
@@ -1,30 +1,88 @@
-(
-function () {
+(function ($, window) {
+  "use strict";
+
   var init = function () {
     $('#b0').click(onB0Click);
     $('#b1').click(onB1Click);
+    $('#b2').click(onB2Click);
   };
 
   var onB0Click = function (ev) {
     ev.preventDefault();
-    hingewrap($('#serif').find('.container'));
-  };
-
-  var onB1Click = function (ev) {
-    ev.preventDefault();
-    hingewrap($('#sans-serif').find('.container'));
-  };
-
-  var hingewrap = function ($j) {
-    $j.texthinge(
+    $('#serif').find('.container').texthinge(
       {
         callback: function (textfragment, counter) {
-          $(this).html(this.innerHTML.substring(0, textfragment.length) + '<br />' + this.innerHTML.slice(textfragment.length + 1));
+          var text = this.innerHTML;
+          $(this).html(
+            [
+              text.substring(0, textfragment.length), '<br />', 
+              text.slice(textfragment.length)
+            ].join('')
+          );
           return false;
         }
       }
     );
   };
 
+  var onB1Click = function (ev) {
+    var firstline;
+    ev.preventDefault();
+    $('#sans-serif').find('.container').texthinge(
+      {
+        callback: function (textfragment, counter) {
+          console.log('callback: "' + textfragment + '"');
+          if (counter === 0) {
+            firstline = textfragment;
+          }
+          else {
+            this.innerHTML = [
+              firstline, '<br />', 
+              textfragment, '<br />', 
+              this.innerHTML.slice(firstline.length + textfragment.length)
+            ].join('');
+          }
+          return (counter > 0 ? false : true);
+        },
+        finish: function (text, arr) {
+          console.log('finished!');
+          console.log(arr);
+          for (var i = 0, len = arr.length; i < len; ++i) {
+            if (i < len - 1) {
+              console.log('"' + text.substring(arr[i], arr[i + 1]) + '"');
+            }
+            else {
+              console.log('"' + text.slice(arr[i]) + '"');
+            }
+          }
+        }
+      }
+    );
+  };
+
+  var onB2Click = function (ev) {
+    ev.preventDefault();
+    $('#comic-sans').find('.container').texthinge(
+      {
+        finish: function (text, arr) {
+          console.log('finished!');
+          console.log(arr);
+          console.log(text);
+          var result = [];
+          for (var i = 0, len = arr.length; i < len; ++i) {
+            if (i < len - 1) {
+              result.push( text.substring(arr[i], arr[i + 1]));
+              result.push( '<br />');
+            }
+            else {
+              result.push( text.substring(arr[i], arr[i + 1]));
+            }
+          }
+          this.innerHTML = result.join('');
+        }
+      }
+    );
+  };
+
   $(init);
-})();
+})(jQuery, window);
